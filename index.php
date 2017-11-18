d13c266da218ed0c08f52724c282622f994e8730
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-11-18 14:46:31 -0500

    minor edit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ class dbConn{
         try {
             self::$db = new PDO( 'mysql:host=' . CONNECTION .';dbname=' . DATABASE, USERNAME, PASSWORD );
             self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	    echo "Connected successfully to db";
+	    echo "Connected successfully to db <br><br>";
         }
         catch (PDOException $e) {
             echo "Connection Error: " . $e->getMessage();
